0e6bfcda1fcd2f66bbb750e4ad346f02c911b8ab
Ryan <93084725+jobmeld@users.noreply.github.com>
2022-12-27 10:05:53 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -66,30 +66,22 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
             </div>
             <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
+                 <a className="navbar-item" title="instagram" href="https://www.instagram.com/cmvadventures/" rel="noopener noreferrer">
+                  <span className="icon">
+                  <img
+                    src={instagram}
+                    alt="Instagram"
+                  />
+                  </span>
+                </a>
             </div>
           </div>
         </div>
