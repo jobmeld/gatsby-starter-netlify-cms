9acad41dcd1749326f92cc6b4b392a77267d7c55
Ryan <93084725+jobmeld@users.noreply.github.com>
2022-12-27 10:09:35 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
-import github from "../img/github-icon.svg";
+import instagram from "../img/social/instagram.svg";
 import logo from "../img/logo.svg";
 
 const Navbar = class extends React.Component {
