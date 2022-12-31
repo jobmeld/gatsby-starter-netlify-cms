2a7f8335fc81f40b910c7155cfcc9637a473dccb
Ryan <93084725+jobmeld@users.noreply.github.com>
2022-12-31 12:07:23 -0500

    asd

--- src/components/FullWidthImage.js
+++ src/components/FullWidthImage.js
@@ -71,9 +71,7 @@ export default function FullWidthImage(props) {
               <h1
                 className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
                 style={{
-                  boxShadow:
-                    "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
-                  backgroundColor: "#888",
+                  backgroundColor: "rgba(0, 0, 0, 0.2)",
                   color: "white",
                   lineHeight: "1",
                   padding: "0.25em",
@@ -86,9 +84,7 @@ export default function FullWidthImage(props) {
               <h3
                 className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
                 style={{
-                  boxShadow:
-                    "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
-                  backgroundColor: "#888",
+                  backgroundColor: "rgba(0, 0, 0, 0.2)",
                   color: "white",
                   lineHeight: "1",
                   padding: "0.25rem",
