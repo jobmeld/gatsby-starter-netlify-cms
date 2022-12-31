99e8a5b039736d16d69e9b00e34b429349d2da0a
Ryan <93084725+jobmeld@users.noreply.github.com>
2022-12-31 12:37:01 -0500

    asdas

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -14,9 +14,7 @@ class BlogRollTemplate extends React.Component {
           posts.map(({ node: post }) => (
             <div className="is-parent column is-6" key={post.id}>
               <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
+                className={`blog-list-item tile is-child box notification`}
               >
                 <header>
                   {post.frontmatter.featuredimage ? (
@@ -93,7 +91,6 @@ export default function BlogRoll() {
                   title
                   templateKey
                   date(formatString: "MMMM DD, YYYY")
-                  featuredpost
                   featuredimage {
                     childImageSharp {
                       gatsbyImageData(
